fa916aff8414bff9b57d07aab535e43bbcd7c569
IlyaSyssa <ilya.syssa@gmail.com>
2024-08-09 13:27:51 +0500

    заного пересобрал компонент и добавил фото

--- diary/src/App.vue
+++ diary/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <!-- <ion-router-outlet /> -->
+    <Header />
   </ion-app>
 </template>
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import Header from './components/Header.vue';
 </script>
