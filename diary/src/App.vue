1ea5b44a15c1313634d08c16e85a616333b8e4ff
IlyaSyssa <ilya.syssa@gmail.com>
2024-08-09 13:11:05 +0500

    добавил компонент и привязал header убрав все остальные компоненты прявязанные к app.vue так как они багали и ломали сайт

--- diary/src/App.vue
+++ diary/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <Header />
   </ion-app>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import Header from './components/Header.vue';
 </script>
